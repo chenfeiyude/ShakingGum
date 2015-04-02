93d208fde009d26505d2e08dc7424a56da38c3e8
Xiang Xu <Xiang@XiangXus-MacBook-Air.local>
2015-04-02 11:46:22 +0100

    Fixed crash loaction

--- ShakingGum.spritebuilder/Gameplay.h
+++ ShakingGum.spritebuilder/Gameplay.h
@@ -9,5 +9,5 @@
 #import "CCNode.h"
 
 
-@interface Gameplay : CCNode<CCPhysicsCollisionDelegate>
+@interface Gameplay : CCNode
 @end
--- ShakingGum.spritebuilder/Gameplay.m
+++ ShakingGum.spritebuilder/Gameplay.m
@@ -45,7 +45,6 @@
     itemManager = [ItemManager getInstance];
     
     [_gum getGumHead].physicsBody.collisionType = @"GumHead";
-    _physicsNode.collisionDelegate = self;
     
     [self schedule:@selector(addItems:) interval:1];
     
@@ -61,12 +60,10 @@
 {
     CGPoint touchLocation = [touch locationInNode:_gum];
     
-    NSLog(@"Touched loaction: %@", NSStringFromCGPoint(touchLocation));
 
     // when a touch insdie of the Gum body occurs
     if (CGRectContainsPoint([[_gum getGumHead] boundingBox], touchLocation))
     {
-    
     }
     
     if(CGRectContainsPoint([[_gum getGumBody] boundingBox], touchLocation))
@@ -98,48 +95,6 @@
     }
 }
 
--(void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair GumHead:(CCNode *)nodeA wildcard:(CCNode *)nodeB
-{
-//    CCLOG(@"Something collided with GumHead!");
-//
-//    for (id obj in _physicsNode.children) {
-//        if ([obj isKindOfClass:[Item class]]) {
-//            Item* item = (Item* ) obj;
-//            
-//            //check crashing here --------------------
-//            if (CGRectIntersectsRect(item.boundingBox, [[_gum getGumHead] boundingBox]))
-//            {
-//                NSLog(@"crashing");
-//                
-//                CCParticleSystem *exploding = [item crashing];
-//                exploding.position = [_gum convertToWorldSpace:item.position];
-//                
-//                [self addChild:exploding];
-//                //
-//                //            [item getStatus];// add status to gum
-//                [item killItem];
-//                [item removeFromParentAndCleanup:YES];
-//                
-//                [self updateScore];
-//                
-//                break;
-//            }
-//            //----------------------------------------
-//            
-//            //check dead items here -------------------
-//            if (item.position.y <= 0) {
-//                //delete all items out of screen
-//                [item killItem];
-//                [item removeFromParentAndCleanup:YES];
-//                break;
-//            }
-//            //-----------------------------------------
-//        }
-//        
-//    }
-//    
-//    [itemManager deleteDeadItems];
-}
 
 // This method is running every frame
 - (void)update:(CCTime)delta {
@@ -150,13 +105,20 @@
         if ([obj isKindOfClass:[Item class]]) {
             Item* item = (Item* ) obj;
             for (CCNode * itemObj in item.children) {
+                
                 CGRect itemObjBoundingbox = itemObj.boundingBox;
                 itemObjBoundingbox.origin = [item convertToWorldSpace:itemObj.position];
+                
                 CGRect headBoundingbox = [[_gum getGumHead] boundingBox];
                 headBoundingbox.origin = [_gum convertToWorldSpace:[_gum getGumHead].position];
+                headBoundingbox.origin.y = headBoundingbox.origin.y - 50;
+                
                 //check crashing here --------------------
                 if (CGRectIntersectsRect(itemObjBoundingbox, headBoundingbox))
                 {
+                    
+                    NSLog(@"item location: %@", NSStringFromCGRect(itemObjBoundingbox));
+                    NSLog(@"head location: %@", NSStringFromCGRect(headBoundingbox));
                 
                     //should have only one obj in an item
                     
